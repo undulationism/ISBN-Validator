8d41f1c33a700bcd10e0f4eef31a77869476e814
FB <>
2018-09-28 22:52:09 +0200

    isbn10 conversion added

--- Program.cs
+++ Program.cs
@@ -3,7 +3,35 @@ using System;
 namespace ISBN_Validator {
     class Program {
         static void Main (string[] args) {
-            Console.WriteLine ("Hello World!");
+            //String input;
+            //String sanitized;
+
+            //Console.WriteLine ("ISBN zur Prüfung eingeben und mit Enter bestätigen:");
+            //input = Console.ReadLine ();
+            //foreach (char c in input) {
+                //if (Char.IsNumber(c));
+            //} 
+            Console.WriteLine(Isbn10("173483927"));
+            Console.WriteLine(Isbn10("857391042"));
+            Console.WriteLine(Isbn10("957391042"));
+
+            
+
+        }
+
+        static String Isbn10 (String incomplete) {
+            int parity = 0;
+            String isbn;
+            for (int i = 0; i < 9; i++) {
+                parity = parity + Convert.ToInt32(Convert.ToString(incomplete[i])) * (i+1);
+            }
+            parity = parity % 11;
+            if (parity == 10) {
+                isbn = incomplete + "X";
+            } else {
+                isbn = incomplete + Convert.ToString(parity);
+            }
+            return isbn;
         }
     }
 }
