eefe3002429db15ec25273ec3d95d3861b865a2e
FB <>
2018-09-28 23:21:12 +0200

    names

--- Program.cs
+++ Program.cs
@@ -2,34 +2,21 @@ using System;
 
 namespace ISBN_Validator {
     class Program {
-        static void Main (string[] args) {
-            //String input;
-            //String sanitized;
-
-            //Console.WriteLine ("ISBN zur Prüfung eingeben und mit Enter bestätigen:");
-            //input = Console.ReadLine ();
-            //foreach (char c in input) {
-                //if (Char.IsNumber(c));
-            //} 
-            Console.WriteLine(Isbn10("173483927"));
-            Console.WriteLine(Isbn10("857391042"));
-            Console.WriteLine(Isbn10("957391042"));
-
-            
+        static void Main (string[] args) {            
 
         }
 
-        static String Isbn10 (String incomplete) {
-            int parity = 0;
+        static String AddCheckDigitToIsbn10 (String isbnWithoutCheckDigit) {
+            int checkSum = 0;
             String isbn;
             for (int i = 0; i < 9; i++) {
-                parity = parity + Convert.ToInt32(Convert.ToString(incomplete[i])) * (i+1);
+                checkSum = checkSum + Convert.ToInt32(Convert.ToString(isbnWithoutCheckDigit[i])) * (i+1);
             }
-            parity = parity % 11;
-            if (parity == 10) {
-                isbn = incomplete + "X";
+            checkSum = checkSum % 11;
+            if (checkSum == 10) {
+                isbn = isbnWithoutCheckDigit + "X";
             } else {
-                isbn = incomplete + Convert.ToString(parity);
+                isbn = isbnWithoutCheckDigit + Convert.ToString(checkSum);
             }
             return isbn;
         }
