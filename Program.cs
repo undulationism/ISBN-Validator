539697b07e5d3082300bfbae6e6dde56fc73e9d9
FB <>
2018-10-05 09:56:03 +0200

    ISBN 13

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ using System;
 namespace ISBN_Validator {
     class Program {
         static void Main (string[] args) {            
-
+            System.Console.WriteLine(AddCheckDigitToIsbn13("978383621997"));
+            System.Console.WriteLine(AddCheckDigitToIsbn13("978245362733"));
         }
 
         static String AddCheckDigitToIsbn10 (String isbnWithoutCheckDigit) {
@@ -19,6 +20,24 @@ namespace ISBN_Validator {
                 isbn = isbnWithoutCheckDigit + Convert.ToString(checkSum);
             }
             return isbn;
+        }
+
+        static String AddCheckDigitToIsbn13 (String isbnWithoutCheckDigit) {
+            int checkSum = 0;
+            int checkDigit;
+            int gewicht = 1;
+
+            for (int i = 0; i < 12; i++) {
+                checkSum = checkSum + Convert.ToInt32(Convert.ToString(isbnWithoutCheckDigit[i])) * (gewicht);
+                if (gewicht == 1) {
+                    gewicht = 3;
+                } else {
+                    gewicht = 1;
+                }
+            }
+            checkDigit = 10 - checkSum % 10;
+
+            return isbnWithoutCheckDigit + checkDigit;
         }
     }
 }
