a0e394a88e82292ea9bbecbc18ee32604acce8ce
FB <>
2018-10-08 12:19:39 +0200

    test12

--- ISBN.cs
+++ ISBN.cs
@@ -2,22 +2,65 @@ using System;
 
 namespace ISBN_Validator {
     class ISBN {
-        static void Main (string[] args) {            
-            System.Console.WriteLine(AddCheckDigitToIsbn13("978383621997"));
-            System.Console.WriteLine(AddCheckDigitToIsbn13("978245362733"));
+        static void Main (string[] args) {
+            while (true) {
+                string test = Console.ReadLine ();
+                test = removeInvalidChars (test);
+                if (test.Length == 10) {
+                    Console.WriteLine (test.Equals (AddCheckDigitToIsbn10 (test.Substring (0, 9))));
+                }
+                if (test.Length == 13) {
+                    Console.WriteLine (test.Equals (AddCheckDigitToIsbn13 (test.Substring (0, 12))));
+                }
+                if (test.Length == 9) {
+                    Console.WriteLine (AddCheckDigitToIsbn10 (test));
+                }
+                if (test.Length == 12) {
+                    Console.WriteLine (AddCheckDigitToIsbn13 (test));
+                }
+            }
+        }
+
+        static string removeInvalidChars (String input_string) {
+            string onlyDigitsAndX = "";
+            string isbn = "";
+            input_string = input_string.ToUpper ();
+
+            foreach (char c in input_string) {
+                if (System.Char.IsDigit (c) || c == 'X') {
+                    onlyDigitsAndX += c;
+                }
+            }
+
+            if (onlyDigitsAndX.Length < 1) {
+                return onlyDigitsAndX;
+            }
+
+            foreach (char c in onlyDigitsAndX.Substring (0, onlyDigitsAndX.Length - 1)) {
+                if (System.Char.IsDigit (c)) {
+                    isbn += c;
+                }
+            }
+            isbn += onlyDigitsAndX[onlyDigitsAndX.Length - 1];
+            if (isbn.Length == 13) {
+                if (isbn[12] == 'X') {
+                    return isbn.Substring (0, 12);
+                }
+            }
+            return isbn;
         }
 
         static String AddCheckDigitToIsbn10 (String isbnWithoutCheckDigit) {
             int checkSum = 0;
             String isbn;
             for (int i = 0; i < 9; i++) {
-                checkSum = checkSum + Convert.ToInt32(Convert.ToString(isbnWithoutCheckDigit[i])) * (i+1);
+                checkSum = checkSum + Convert.ToInt32 (Convert.ToString (isbnWithoutCheckDigit[i])) * (i + 1);
             }
             checkSum = checkSum % 11;
             if (checkSum == 10) {
                 isbn = isbnWithoutCheckDigit + "X";
             } else {
-                isbn = isbnWithoutCheckDigit + Convert.ToString(checkSum);
+                isbn = isbnWithoutCheckDigit + Convert.ToString (checkSum);
             }
             return isbn;
         }
@@ -28,7 +71,7 @@ namespace ISBN_Validator {
             int gewicht = 1;
 
             for (int i = 0; i < 12; i++) {
-                checkSum = checkSum + Convert.ToInt32(Convert.ToString(isbnWithoutCheckDigit[i])) * (gewicht);
+                checkSum = checkSum + Convert.ToInt32 (Convert.ToString (isbnWithoutCheckDigit[i])) * (gewicht);
                 if (gewicht == 1) {
                     gewicht = 3;
                 } else {
